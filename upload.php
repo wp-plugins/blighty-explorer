ece9353e30333c30166fb4f7d1dabce8306a13a5
Blighty <Blighty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-08 17:02:11 +0000

    1.4.0
    git-svn-id: https://plugins.svn.wordpress.org/blighty-explorer/tags/1.4.0@1176891 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ upload.php
@@ -0,0 +1,98 @@
+<?php
+/**
+
+Copyright (C) 2015 Chris Murfin
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+**/
+
+function bex_upload( $atts ) {
+	if (!empty($_GET["folder"])) {
+		$folder = esc_attr($_GET["folder"]);
+		$folder = ltrim($folder, ".");
+	} else {
+		$folder = "";
+	}
+
+	$out = '<form id="bexUpload" class="bex-upload-form" action="' .admin_url( 'admin-ajax.php' ) .'" method="post" enctype="multipart/form-data">';
+	$out .= '  Select file to upload:';
+	$out .= '  <input type="file" name="bexFile" id="bexFile">';
+	$out .= '  <input type="hidden" name="bexFolder" id="bexFolder" value="' .$folder .'">';
+	$out .= '  <input type="submit" value="Upload" name="bexSubmit" id="bexSubmit" class="bex-buttom-submit">';
+	$out .= '<br /><br /><div class="bex-progress">';
+	$out .= '  <div class="bex-bar"></div >';
+	$out .= '  <div class="bex-percent">0%</div >';
+	$out .= '</div><br />';
+    $out .= '<div id="bexStatus">Ready to upload.</div>';
+	$out .= '</form>';
+
+	return $out;
+}
+
+function bex_submission_processor_nopriv() {
+	die();
+}
+
+function bex_submission_processor() {
+	
+	if(empty($_FILES["bexFile"])) {
+		echo "No file selected.";
+		die();
+	}
+	
+	global $dropbox;
+	
+	$access_token = bex_load_token("access");
+	if(empty($access_token)) {
+		echo 'Dropbox not authorized. Check the settings for Blighty Explorer (bex_upload - 1)';
+		die();
+	}
+	
+	$dropbox->SetAccessToken($access_token);
+	if (!$dropbox->IsAuthorized()) {
+		echo 'Dropbox not authorized. Check the settings for Blighty Explorer (bex_upload - 2)';
+		die();
+	}
+	
+	$rootFolder = trailingslashit(get_option('bex_folder'));
+	if (get_option('bex_allow_uploads')) {
+		$folder = esc_attr($_POST["bexFolder"]);
+	} else {
+		$folder = UPLOADS_FOLDER;
+	}
+	
+	$workingFolder = trailingslashit($rootFolder .$folder);
+
+	if (!empty($_POST['bexSubmit'])) {
+		$dropbox->UploadFile($_FILES["bexFile"]["tmp_name"], $workingFolder .$_FILES["bexFile"]["name"], false);
+		echo 'File uploaded.';
+	}
+	
+	if (get_option('bex_email_upload')) {
+		global $current_user;
+    	get_currentuserinfo();
+
+		$headers = 'From: ' .get_bloginfo('name') .' <' .get_bloginfo('admin_email') .'>' . "\r\n";
+		$subj = 'File uploaded from ' .get_bloginfo('name');
+		$body = 'The file "' .$_FILES["bexFile"]["name"] .'" has just been uploaded by ' .$current_user->user_login
+				.' (' .$current_user->user_email .')';
+		wp_mail( get_bloginfo('admin_email'), $subj, $body, $headers );
+	}
+	
+	die();
+
+}
+
+?>
