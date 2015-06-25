9b97706144cb33f205f21c1fc9aabef5c25d3aac
Blighty <Blighty@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-25 13:07:22 +0000

    1.5.2
    git-svn-id: https://plugins.svn.wordpress.org/blighty-explorer/trunk@1187517 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- upload.php
+++ upload.php
@@ -16,16 +16,11 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-v1.5.1
+v1.5.2
 
 **/
 
 function bex_upload( $atts ) {
-	if (!empty($_GET["file"])) {
-		return '';
-		die();
-	}
-
 	if (!empty($_GET["folder"])) {
 		$folder = esc_attr($_GET["folder"]);
 		$folder = ltrim($folder, ".");
